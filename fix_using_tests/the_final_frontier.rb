67f2c3274055ac25641f6efc5c399d5ea2f5792c
David Chung <dbchung3@yahoo.com>
2020-04-13 18:40:08 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -7,14 +7,16 @@ def generate_star_date
 end
 
 
+def engage
+  puts state_log(date)
+  date = generate_star_date
+end
+
+
 
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
-def engage
-  puts state_log(date)
-  date = generate_star_date
-end
 
 binding.pry
