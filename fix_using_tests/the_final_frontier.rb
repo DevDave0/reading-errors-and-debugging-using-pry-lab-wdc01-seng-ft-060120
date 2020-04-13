fa5a32d77a8ff50e01efcf445b717a592d7f805c
David Chung <dbchung3@yahoo.com>
2020-04-13 18:39:26 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -6,7 +6,6 @@ def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
 
-binding.pry
 
 
 def state_log(star_date)
@@ -17,3 +16,5 @@ def engage
   puts state_log(date)
   date = generate_star_date
 end
+
+binding.pry
